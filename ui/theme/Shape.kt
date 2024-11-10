e14019a52211e8b15a398343a26f57fbafbff7b0
athy <62893375+athyzr@users.noreply.github.com>
2024-11-11 06:10:30 +0700

    Update Shape.kt

--- ui/theme/Shape.kt
+++ ui/theme/Shape.kt
@@ -15,6 +15,7 @@
  */
 package com.example.inventory.ui.theme
 
+/*mendefinisikan shape dalam tema UI menggunakan jetpack composse*/
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
