f5415bff2422e6941e6c07da493a4e83ae172d82
athy <62893375+athyzr@users.noreply.github.com>
2024-11-11 06:14:41 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
-    //Room
+    //menambahkan dependensi library room. ksp adalah API yang bagus untuk megnurai anotasi kotlin
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
